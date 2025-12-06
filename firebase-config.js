31b5c5f7ba97f06e7ce84ba327f5f7237c5c455b
Keep Pushing Love Studios <keeppushinglovestudios@gmail.com>
2025-12-06 09:58:50 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase config (kept local, not committed)
+window.FESTIVAL_MIXER_CONFIG = {
+  apiKey: "",
+  authDomain: "festival-jam-mixer.firebaseapp.com",
+  projectId: "festival-jam-mixer",
+  storageBucket: "festival-jam-mixer.firebasestorage.app",
+  messagingSenderId: "255348637744",
+  appId: "1:255348637744:web:cbd3151011372f0e8fadf5",
+  measurementId: "G-GOZHBW07C3"
+};
