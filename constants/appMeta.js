c8c6342f78e84be0f5fddbb434727756e704596f
Keep Pushing Love Studios <keeppushinglovestudios@gmail.com>
2026-01-29 17:07:55 -0600

    Add feature flags to disable unstable UI

--- constants/appMeta.js
+++ constants/appMeta.js
@@ -10,6 +10,16 @@ const FEATURE_FLAGS = {
   ownedToggle: true
 };
 
+const FEATURES = {
+  OWNED_SYNC: false,
+  PROFILE: false,
+  PREVIEW: false,
+  SANDBOX: false,
+  HELP_POLISH: true
+};
+
+const isFeatureOn = (name) => Boolean(FEATURES?.[name]);
+
 const HELP_CONTENT = {
   version: 2,
   basic: {
@@ -258,6 +268,8 @@ const WHATS_NEW = DEFAULT_WHATS_NEW.items;
 window.APP_VERSION = APP_VERSION;
 window.WHATS_NEW = WHATS_NEW;
 window.FEATURE_FLAGS = FEATURE_FLAGS;
+window.FEATURES = FEATURES;
+window.isFeatureOn = isFeatureOn;
 window.HELP_CONTENT = HELP_CONTENT;
 window.DEFAULT_HELP_CONTENT = DEFAULT_HELP_CONTENT;
 window.DEFAULT_WHATS_NEW = DEFAULT_WHATS_NEW;
